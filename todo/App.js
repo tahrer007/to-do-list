d5fdadfd3705d6206a4f4f4e21938a2060ad84e3
tahrer ad <tahrer_Ad@outlook.com>
2022-03-04 16:29:15 +0200

    useReducer

--- todo/App.js
+++ todo/App.js
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler";
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,18 +7,24 @@ import IndexScreen from "./src/screens/indexScreen";
 import { BlogContext } from "./src/context/BlogContext";
 
 const Stack = createStackNavigator();
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogPost":
+      return [...state, { title: ` blog post # ${state.length + 1}` }];
+
+    default:
+      return state;
+  }
+};
 
 const app = () => {
-  const [BlogPosts, setBlogPost] = useState([]);
+  const [BlogPosts, dispatch] = useReducer(blogReducer, []);
   const addBlogPost = () => {
-    setBlogPost([
-      ...BlogPosts,
-      { title: `blog post  ${BlogPosts.length + 1}` },
-    ]);
+    dispatch({ type: "add_blogPost" });
   };
 
   return (
-    <BlogContext.Provider value={{ data: BlogPosts, addBlogPost: addBlogPost }}>
+    <BlogContext.Provider value={{ data: BlogPosts, addBlogPost }}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="index">
           <Stack.Screen
